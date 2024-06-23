c69b39dfdc6c28a305c0b5ff185adabbcf84e7d5
Suman Kisku <sumankisku1@gmail.com>
2024-06-23 19:12:15 +0530

    auth,authorization, user controllers

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,10 @@ import { Application, Request, Response } from "express"
 
 import express from "express";
 import cors from "cors";
-import userRouter from "./routes/user"
+import userRouter from "./routes/user.route"
 import { connectDb, connectSession } from "./connections";
+import { isAuthenticated } from "./middlewares/authentication.middle";
+import { isRoleCanditate } from "./middlewares/authrization.middle";
 
 require("dotenv").config();
 
@@ -11,11 +13,15 @@ const app: Application = express();
 const port = 3000;
 const mongo_url = process.env.MONGO_URL as string;
 
-app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+}));
 app.use(express.json());
 
 connectDb(mongo_url);
 app.use(connectSession(mongo_url));
+// app.use(isAuthenticated);
 
 app.get("/", (req: Request, res: Response) => {
     res.json({
@@ -24,8 +30,14 @@ app.get("/", (req: Request, res: Response) => {
     })
 })
 
-app.use("/api/user", userRouter)
+app.get("/private", isAuthenticated, isRoleCanditate, (req: Request, res: Response) => {
+    return res.status(200).json({
+        status: "ok",
+        message: "You're authenticated",
+    })
+})
 
+app.use("/api/user", userRouter)
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
