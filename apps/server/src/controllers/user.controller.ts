f0b4f305777f64dce02c20c2ca88f9786fbcbe3b
Suman Kisku <sumankisku1@gmail.com>
2024-07-12 00:19:48 +0530

    server had problem fetching from packages, so declared to schema in the file itself as before

--- apps/server/src/controllers/user.controller.ts
+++ apps/server/src/controllers/user.controller.ts
@@ -1,11 +1,29 @@
-import { ZodError } from "zod";
+import { z, ZodError } from "zod";
 import bcrypt from "bcrypt";
 
 import User from "../models/user.model";
 import { Request, Response } from "express";
-const { formSchema } = require("zod-schemas");
 
 const saltRounds = 10;
+const formSchema = z
+    .object({
+        name: z
+            .string()
+            .min(4, {
+                message: "Name must be at least 4 characters",
+            })
+            .max(50),
+        email: z.string().email().min(5),
+        password: z.string().min(8),
+        confirm: z.string().min(8),
+        role: z.string({
+            required_error: "Please select a role.",
+        }),
+    })
+    .refine((data) => data.password === data.confirm, {
+        message: "Passwords doesn't match",
+        path: ["confirm"],
+    });
 
 export const signupUserAsync = async (req: Request, res: Response) => {
     try {
